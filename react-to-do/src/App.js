a8a7492e7da91be3a2549c3e5e02556fd6564cae
Mariel <mariel.capulli@gmail.com>
2018-02-25 13:07:45 -0500

    adding files from parent folder

--- react-to-do/src/App.js
+++ react-to-do/src/App.js
@@ -7,9 +7,9 @@ class App extends Component {
     super(props);
     this.state = {
       todos: [
-        {description: 'Walk the cat', isCompleted: true },
-        {description: 'Throw the dishes away', isCompleted: false },
-        {description: 'Buy new dishes', isCompleted: false }
+        {description: 'Walk the cat ', isCompleted: true },
+        {description: 'Throw the dishes away ', isCompleted: false },
+        {description: 'Buy new dishes ', isCompleted: false }
       ],
       newTodoDescription: ''
     };
@@ -26,19 +26,25 @@ class App extends Component {
     this.setState({ todos: [...this.state.todos, newTodo], newTodoDescription: '' });
   }
 
-    toggleComplete(index) {
-      const todos = this.state.todos.slice();
-      const todo = todos[index];
-      todo.isCompleted = todo.isCompleted ? false : true;
-      this.setState({ todos: todos });
+  toggleComplete(index) {
+    const todos = this.state.todos.slice();
+    const todo = todos[index];
+    todo.isCompleted = todo.isCompleted ? false : true;
+    this.setState({ todos: todos });
     }
+
+  deleteTodo(index) {
+   const newTodos = this.state.todos.filter((todo)=> {return todo !== this.state.todos[index]})
+   this.setState({ todos: newTodos});
+  }
+
 
   render() {
     return (
       <div className="App">
        <ul>
           { this.state.todos.map( (todo, index) =>
-            <ToDo key ={ index } description= { todo.description } isCompleted= { todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
+            <ToDo key ={ index } description= { todo.description } isCompleted= { todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } deleteTodo = { () => this.deleteTodo(index)} />
           )}
        </ul>
        <form onSubmit={ (e) => this.handleSubmit(e)} >
